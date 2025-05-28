354df2d8fc7c6c1cfef674dbf7dbf0e87e751b2f
Abishek2030 <rockyabishek7@gmail.com>
2025-05-28 15:52:05 +0530

    hi.cpp

--- hi.cpp
+++ hi.cpp
@@ -1,9 +1,17 @@
-#include <iostream>
-using namespace std;
+#include<iostream>
+
+class Abishek{
+
+    void print(int a){
+        std::cout<<a;
+    }
+};
 
 int main(){
-  int a=10,b=20;
-  cout<<a+b;
+    int a=10;
+    Abishek abi;
+    abi.print(a);
+
 
     return 0;
 }
